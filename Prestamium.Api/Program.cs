61c407cdfc23cc98915877f6408c23874147942d
Luis Cabal <cabalholguinlufe@gmail.com>
2024-07-10 02:48:12 -0500

    create connection to db, create repository clients and first migration

--- Prestamium.Api/Program.cs
+++ Prestamium.Api/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Prestamium.Persistence;
+using Prestamium.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Configure DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("defaultConnection"));
+});
+
+//register services
+builder.Services.AddScoped<ClientRepository>();
+
 
 var app = builder.Build();
 
---
+++ Prestamium.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Prestamium.Entities;
+
+namespace Prestamium.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        //Fluent Api
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Client>().Property(x => x.Name).HasMaxLength(50);
+        }
+
+        public DbSet<Client> Clients { get; set; }
+    }
+}
